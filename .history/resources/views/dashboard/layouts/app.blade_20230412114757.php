ed51835188942ff91b112909b64f1d1cb1004c5c
MrBoOodY <abdallamohamed197@gmail.com>
2023-04-12 14:03:25 +0200

    git ihnore updated

---
+++ .history/resources/views/dashboard/layouts/app.blade_20230412114757.php
@@ -0,0 +1,29 @@
+ <!DOCTYPE html>
+ <html lang="en">
+
+ <head>
+     <meta charset="utf-8" />
+     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+     <link rel="apple-touch-icon" sizes="76x76" href="{{asset('assets/admin_assets/img/small-logos/logo-spotify.svg')}}">
+     <link rel="icon" type="image/png" href="{{asset('assets/admin_assets/img/small-logos/logo-spotify.svg')}}">
+     <title>
+         Argon Dashboard 2 by Creative Tim
+     </title>
+     <!--     Fonts and icons     -->
+     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+     <!-- Nucleo Icons -->
+     <link href="{{asset('assets/admin_assets/img/small-logos/logo-spotify.svg')}}" />
+     <link href="{{asset('assets/admin_assets/img/small-logos/logo-spotify.svg')}}" />
+     <!-- Font Awesome Icons -->
+     <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+     <link href="{{asset('assets/admin_assets/img/small-logos/logo-spotify.svg')}}" />
+     <!-- CSS Files -->
+     <link id="pagestyle" href="{{asset('assets/admin_assets/img/small-logos/logo-spotify.svg')}}" />
+ </head>
+
+ <body class="g-sidenav-show   bg-gray-100">
+     @yield('body')
+     @yield('script')
+ </body>
+
+ </html>
